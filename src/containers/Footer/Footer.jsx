ede449a24325cb4769c36fe62f2103d1b2124df5
salaxer <contact@salaxer.com>
2023-07-18 23:01:53 -0600

    [updated]: Se agrego el Footer y el Header en nuevas carpetas

---
+++ src/containers/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+const Footer = () =>{
+    return (
+        <section className="Footer">
+            <p className="Footer__text">Made with ❤ by Salaxer</p>
+            <p className="Footer__text">Last Update on June 2022</p>
+        </section>
+    )
+}
+export default Footer;
---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useMemo, useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
+import NavDesktop from '../../components/NavDesktop'
+import NavMobile from '../../components/NavMobile';
+
+import './header.css';
+import MediaQuery from '../../utils/MediaQuery';
+
+const Header = () =>{
+    
+    const refHeader = useRef(null);
+
+    const rest = MediaQuery();
+
+    const [navi, setNavi] = useState(false);
+
+    useMemo(()=>{
+        if (rest === 'mobile') {
+            setNavi(true)
+        }else{
+            setNavi(false)
+        }
+    },[rest])
+
+    useEffect(()=>{
+        const computed = window.getComputedStyle(refHeader.current).getPropertyValue("backdrop-filter");
+        if (computed) return;
+        if (!computed) {
+            refHeader.current.style.backgroundColor = "var(--maincolor)";
+        }
+    },[refHeader])
+
+    return (
+        <>
+            <nav ref={refHeader} className="Header">
+                <h1 className="Logo">SALAXER</h1>
+                {navi ? null : <NavDesktop/>}
+            </nav>
+            {navi ? <NavMobile/> : null }
+        </>
+    )
+}
+export default Header;
